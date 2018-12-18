2b83d2e047948a56091e2216ab69c99e4079d36a
Mcqun <mchqun@126.com>
2018-12-18 22:01:14 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 if [ '`ls /app|wc -l`' != '0' ]; then
-  echo $GIT_URL
-  echo $GIT_BRANCH
-  echo $TEMP_DIR
   ls
   if [ $GIT_URL != null ]; then
   echo '----开始下载代码----'
-  #git clone $GIT_URL $TEMP_DIR -b $GIT_BRANCH
-  #mv -f $TEMP_DIR/* $TEMP_DIR/.[^.]* /app
-  #rm -rf $TEMP_DIR
+  git clone $GIT_URL $TEMP_DIR -b $GIT_BRANCH
+  mv -f $TEMP_DIR/* $TEMP_DIR/.[^.]* /app
+  rm -rf $TEMP_DIR
   echo '----代码下载完毕----'
-  #composer install
-  #php think swoole
+  composer install
+  php think swoole
   else
     echo '----GIT_URL未设置----'
   fi
