981bd113c9acb1f2f7a234ce7039b33c1d066aae
Mcqun <mchqun@126.com>
2019-01-05 20:09:18 +0800

    update

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ ls -ld /app
 ls -A /app
 echo '----文件数量：----'
 ls -A /app|wc -w
-if [ '`ls -A /app|wc -w`' = '0' ]; then
+if [ `ls -A /app|wc -w` = 0 ]; then
   if [ $GIT_URL != null ]; then
     echo '-----清空APP文件夹----'
     rm -rf /app/* /app/.[^.]*
@@ -13,7 +13,7 @@ if [ '`ls -A /app|wc -w`' = '0' ]; then
     mv -f $TEMP_DIR/* $TEMP_DIR/.[^.]* /app
     rm -rf $TEMP_DIR
     echo '----代码下载完毕----'
-    if [ '`ls -A /app|wc -w`' != '0' ]; then
+    if [ `ls -A /app|wc -w` > 0 ]; then
       ls -A /app
       composer install
       php think swoole
